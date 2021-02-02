ed738cd1e7fdc872c2395cffe6e16e269c6d8020
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-02-02 19:58:30 +0100

    responsive start

--- content-featured.php
+++ content-featured.php
@@ -1,18 +1,6 @@
 <?php $format = get_post_format(); ?>
 
 <div class="featured-item">
-	<div class="featured-item-left">
-		<div class="featured-item-bottom">
-			<div class="featured-item-avatar"><?php echo get_avatar(get_the_author_meta('user_email'),'128'); ?></div>
-			<h2 class="featured-item-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<div class="featured-item-category"><?php the_category(' / '); ?></div>
-			<?php if (get_theme_mod('excerpt-length','16') != '0'): ?>
-				<div class="featured-item-excerpt">
-					<?php the_excerpt(); ?>
-				</div>
-			<?php endif; ?>
-		</div>
-	</div>
 	<div class="featured-item-right" href="<?php the_permalink(); ?>" style="background-image:url('<?php the_post_thumbnail_url('folias-square'); ?>');">
 		<a class="featured-item-link" href="<?php the_permalink(); ?>" rel="bookmark"></a>
 		<div class="type-portfolio-date">
@@ -26,4 +14,17 @@
 			</div>
 		<?php endif; ?>
 	</div>
+	<div class="featured-item-left">
+		<div class="featured-item-bottom">
+			<div class="featured-item-avatar"><?php echo get_avatar(get_the_author_meta('user_email'),'128'); ?></div>
+			<h2 class="featured-item-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<div class="featured-item-category"><?php the_category(' / '); ?></div>
+			<?php if (get_theme_mod('excerpt-length','16') != '0'): ?>
+				<div class="featured-item-excerpt">
+					<?php the_excerpt(); ?>
+				</div>
+			<?php endif; ?>
+		</div>
+	</div>
+	
 </div>
