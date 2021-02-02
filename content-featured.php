68b32ba600df398af4255d6e721ba1ceffeefe7b
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-02-02 16:21:57 +0100

    featured content

--- content-featured.php
+++ content-featured.php
@@ -1,21 +1,29 @@
 <?php $format = get_post_format(); ?>
 
-<div class="featured-item-wrap">
-	<div class="featured-item-left">Left</div>
-	<div class="featured-item-right">right</div>
-	<div class="featured-item" style="background-image:url('<?php the_post_thumbnail_url('folias-mediumx'); ?>');">
-		<?php if ( has_post_format('video') && !is_sticky() ) echo'<span class="thumb-icon small"><i class="fas fa-play"></i></span>'; ?>
-		<?php if ( has_post_format('audio') && !is_sticky() ) echo'<span class="thumb-icon small"><i class="fas fa-volume-up"></i></span>'; ?>
-		<?php if ( is_sticky() ) echo'<span class="thumb-icon small"><i class="fas fa-star"></i></span>'; ?>
-		<a class="featured-link" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-		<div class="featured-content">
-			<h3 class="featured-title"><?php the_title(); ?></h3>
+<div class="featured-item">
+	<div class="featured-item-left">
+		<div class="featured-item-bottom">
+			<div class="featured-item-avatar"><?php echo get_avatar(get_the_author_meta('user_email'),'128'); ?></div>
+			<h2 class="featured-item-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<div class="featured-item-category"><?php the_category(' / '); ?></div>
+			<?php if (get_theme_mod('excerpt-length','26') != '0'): ?>
+				<div class="featured-item-excerpt">
+					<?php the_excerpt(); ?>
+				</div>
+			<?php endif; ?>
 		</div>
-		<?php if ( comments_open() && ( get_theme_mod( 'comment-count', 'on' ) == 'on' ) ): ?>
-			<a class="post-comments" href="<?php comments_link(); ?>"><span><?php comments_number( '0', '1', '%' ); ?></span></a>
-		<?php endif; ?>	
 	</div>
-	<div class="featured-item-date">
-		<i class="far fa-calendar"></i> <?php the_time( get_option('date_format') ); ?>
+	<div class="featured-item-right" href="<?php the_permalink(); ?>" style="background-image:url('<?php the_post_thumbnail_url('folias-mediumx'); ?>');">
+		<a class="featured-item-link" href="<?php the_permalink(); ?>" rel="bookmark"></a>
+		<div class="type-portfolio-date">
+			<div class="type-portfolio-date-month"><?php the_time('M') ?></div>
+			<div class="type-portfolio-date-day"><?php the_time('d') ?></div>
+			<div class="type-portfolio-date-year"><?php the_time('Y') ?></div>
+		</div>
+		<?php if ( get_theme_mod('comment-count','on') == 'on' ): ?>
+			<div class="type-portfolio-comments">
+				<a class="post-comments" href="<?php comments_link(); ?>"><span><?php comments_number( '0', '1', '%' ); ?></span></a>		
+			</div>
+		<?php endif; ?>
 	</div>
 </div>
