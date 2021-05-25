6f451c24375aa9c40aa54bab4be772ec1203a04d
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-05-25 14:23:16 +0200

    1.0.2

--- functions.php
+++ functions.php
@@ -98,8 +98,10 @@ add_action( 'after_setup_theme', 'folias_setup' );
 
 /*  Custom navigation
 /* ------------------------------------ */
-add_action( 'wp', function() {
+if ( ! class_exists( '\Folias\Nav' ) ) {
 	require_once 'functions/nav.php';
+}
+add_action( 'wp', function() {
 	$nav = new \Folias\Nav();
 	$nav->enqueue(
 		[
