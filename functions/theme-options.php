1639e012804e808654b24f951c9ccc9030824560
AlxMedia <contact@alx.media>
2022-02-23 12:05:41 +0100

    Update FontAwesome customizer link to v5 brands

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -569,7 +569,7 @@ Kirki::add_field( 'folias_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'folias' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'folias' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'folias' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'folias' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'folias' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'folias' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'folias' ),
@@ -586,7 +586,7 @@ Kirki::add_field( 'folias_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'folias' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'folias' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'folias' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'folias' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'folias' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
